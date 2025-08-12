fa5cbb9d3b6f9ac575773ae916c46d093066c7bc
Aman-18S <130240004+Aman-18S@users.noreply.github.com>
2025-08-12 16:26:25 +0530

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello, Aman — from Netlify Function!" })
+    body: JSON.stringify({ message: "Hello from Netlify Backend" })
   };
 };
